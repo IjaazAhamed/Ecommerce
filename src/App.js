c3b77c45099826b6d8d8f9e3d7aca85cdcafe198
IjaazAhamed <ijaazijju1713@gmail.com>
2022-07-07 07:47:21 +0530

    Picture Fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Product from "./components/header/Product";
+import Landingpage from "./components/Landingpage/Landingpage";
+
+function App() {
+  return (
+    <div className="App">
+      <Landingpage />
+    </div>
+  );
+}
+
+export default App;
